a0537c8aa4be397d5718ecaaf34a299ae3025aef
Alfonso <alsolarapole@gmail.com>
2025-10-01 11:25:38 +0800

    Add padding

--- app/(patient)/_layout.tsx
+++ app/(patient)/_layout.tsx
@@ -1,6 +1,7 @@
 import { Tabs } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Platform, StatusBar } from 'react-native';
 
 export default function PatientLayout() {
   return (
@@ -27,6 +28,9 @@ export default function PatientLayout() {
           headerTintColor: '#FFFFFF',
           headerTitleStyle: {
             fontWeight: 'bold',
+          },
+          sceneStyle: {
+            paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
           },
         }}
       >
